1a6db4b54ae786773d6afa2e4b63101dca1f8415
natnaelh14 <natnaelh14@github.com>
2023-01-05 11:16:41 -0500

    optimize contentful preview mode

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getProduct } from '../../utils/products';
+import * as contentful from '../../utils/contentful';
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,6 +10,13 @@ export default async function handler(
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !productId) {
     return res.status(401).json({ message: 'Invalid token' });
   }
+  // @ts-ignore
+  const product = await getProduct(contentful.client, productId);
+  if (!product.items.length) {
+    return res.status(401).json({ message: 'Invalid productId' });
+  }
+  const verifiedProductId = product.items[0].fields?.productId || '';
+
   res.setPreviewData({});
-  res.redirect(`/products/${productId}`);
+  res.redirect(`/products/${verifiedProductId}`);
 }
